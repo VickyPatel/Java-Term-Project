0713064691d2c732fd2cb2350e5a0109c6658660
Vicky Patel <vicky.7676@yahoo.in>
2015-03-25 02:37:25 -0300

    Try to Insert Register Page

---
+++ src/main/java/personDetail/Person.java
@@ -0,0 +1,50 @@
+package personDetail;
+
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+
+import javax.faces.bean.ManagedBean;
+
+
+
+/**
+ *
+ * @author c0633648
+ */
+@ManagedBean
+public class Person {
+    public String name;
+    public String email;
+    public String password;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+    
+    
+    
+}
